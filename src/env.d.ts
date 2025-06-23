db1c98d8b146cceb9087f16416857886edc09382
heinNell <heinnell64@gmail.com>
2025-06-23 17:11:09 +0000

    feat: configure Firebase with environment variables and deploy

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_API: string;
   readonly VITE_FIREBASE_AUTH_DOMAIN: string;
   readonly VITE_FIREBASE_DATABASE_URL: string;
   readonly VITE_FIREBASE_PROJECT_ID: string;
