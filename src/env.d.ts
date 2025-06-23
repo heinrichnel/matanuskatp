47336ab235deccdd8bfb68eb6487a8478830ceed
heinNell <heinnell64@gmail.com>
2025-06-23 17:14:34 +0000

    feat: final check and deploy

--- src/env.d.ts
+++ src/env.d.ts
@@ -9,7 +9,6 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly VITE_FIREBASE_APP_ID: string;
   readonly VITE_FIREBASE_MEASUREMENT_ID?: string; // Optional as measurement is not always used
-  readonly VITE_FIREBASE_DATABASE_ID?: string; // Added database ID
 }
 
 interface ImportMeta {
