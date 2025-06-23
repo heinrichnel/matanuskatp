fc157740c99e19ac85a14f82fa7c8cc43608b00f
heinNell <heinnell64@gmail.com>
2025-06-23 17:11:09 +0000

    feat: configure Firebase with environment variables and deploy

--- src/firebase.ts
+++ src/firebase.ts
@@ -29,14 +29,14 @@ interface FirebaseConfig {
 }
 
 const firebaseConfig: FirebaseConfig = {
-  apiKey: "AIzaSyBtq7Z6qqaVmb22d3aNcwNiqkrbGtIhJ7g",
-  authDomain: "mat1-9e6b3.firebaseapp.com",
-  databaseURL: "https://mat1-9e6b3-default-rtdb.firebaseio.com",
-  projectId: "mat1-9e6b3",
-  storageBucket: "mat1-9e6b3.firebasestorage.app",
-  messagingSenderId: "250085264089",
-  appId: "1:250085264089:web:51c2b209e0265e7d04ccc8",
-  measurementId: "G-YHQHSJN5CQ"
+  apiKey: import.meta.env.VITE_FIREBASE_API || '',
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || '',
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || '',
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || '',
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || '',
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '',
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || '',
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || ''
 };
 
 // Initialize Firebase
