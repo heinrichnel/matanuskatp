21a25f550ce0f440dfa713d85e0e1c76a3413f43
heinNell <heinnell64@gmail.com>
2025-07-05 09:11:00 +0000

    Update environment variables and Firebase configurations

--- src/env.d.ts
+++ src/env.d.ts
@@ -9,6 +9,25 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly VITE_FIREBASE_APP_ID: string;
   readonly VITE_FIREBASE_MEASUREMENT_ID?: string; // Optional as measurement is not always used
+  readonly VITE_RECAPTCHA_KEY: string;
+  readonly VITE_SDK_SERVICE_ACCOUNT_KEY: string;
+  readonly VITE_FIREBASE_CLOUD_MESSAGING_API: string;
+  readonly VITE_FIREBASE_SENDER_ID: string;
+  readonly VITE_FIREBASE_SERVICE_ACCOUNT: string;
+  readonly VITE_FIREBASE_SERVICE_ACCOUNT_ID: string;
+  readonly VITE_FIREBASE_IMPORTER_SERVICE_ACCOUNT: string;
+  readonly VITE_FIREBASE_IMPORTER_SERVICE_ACCOUNT_ID: string;
+  readonly VITE_FIREBASE_IMPORTER_KEY: string;
+  readonly VITE_FIREBASE_IMAGE_PROCESSING_SERVICE_ACCOUNT: string;
+  readonly VITE_FIREBASE_IMAGE_PROCESSING_KEY: string;
+  readonly VITE_FIREBASE_USER_DOCUMENT_SERVICE_ACCOUNT: string;
+  readonly VITE_FIREBASE_USER_DOCUMENT_SERVICE_ACCOUNT_ID: string;
+  readonly VITE_FIREBASE_USER_DOCUMENT_KEY: string;
+  readonly VITE_FIREBASE_COMPUTE_SERVICE_ACCOUNT: string;
+  readonly VITE_FIREBASE_COMPUTE_SERVICE_ACCOUNT_ID: string;
+  readonly VITE_FIREBASE_COMPUTE_KEY: string;
+  readonly VITE_FIREBASE_ADMIN_SDK_SERVICE_ACCOUNT: string;
+  readonly VITE_FIREBASE_ADMIN_SDK_SERVICE_ACCOUNT_ID: string;
 }
 
 interface ImportMeta {
