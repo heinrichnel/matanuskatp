1db266ba7e7e0844716603b02ddb0f3fbb0e0e7c
heinNell <heinnell64@gmail.com>
2025-07-07 09:49:41 +0200

    Updated Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -318,7 +318,7 @@ const Sidebar: FC<HeaderProps> = ({
           <SyncIndicator showText={true} className="mt-1" />
         </div>
         <div className="flex items-center gap-3 mt-2">
-          <User className="w-5 h-5 text-gray-400" />
+          <Users className="w-5 h-5 text-gray-400" />
           <span className="text-sm text-gray-700">User</span>
         </div>
       </div>
