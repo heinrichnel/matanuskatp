03c86c4cd4d89b8cab6c930b28e8c6c0dc9fc3d6
heinNell <heinnell64@gmail.com>
2025-07-07 05:25:43 +0200

    Add missing React imports to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { AppProvider, useAppContext } from "./context/AppContext";
 import { SyncProvider } from "./context/SyncContext";
