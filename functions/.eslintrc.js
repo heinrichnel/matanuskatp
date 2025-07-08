93f7f9f2fc2ae714c49c698626502f5247d7da09
heinNell <heinnell64@gmail.com>
2025-07-08 22:57:05 +0000

    Fix all TypeScript errors and prepare for deployment

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,26 +1,40 @@
 module.exports = {
+  root: true,
   env: {
-    es6: true,
     node: true,
+    es6: true,
   },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    "ecmaVersion": 2018,
+    ecmaVersion: 2020,
+    sourceType: "module", // Allow import/export
   },
+  plugins: [
+    "@typescript-eslint",
+  ],
   extends: [
     "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "google",
   ],
   rules: {
+    // General
+    "quotes": ["error", "double", { "allowTemplateLiterals": true }],
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "require-jsdoc": "off",
+    "valid-jsdoc": "off",
+    "object-curly-spacing": ["error", "always"],
+    "max-len": ["warn", { "code": 120 }],
+    "indent": ["error", 2],
+    // TypeScript-specific
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
   },
   overrides: [
     {
-      files: ["**/*.spec.*"],
-      env: {
-        mocha: true,
-      },
+      files: ["**/*.spec.*", "**/*.test.*"],
+      env: { mocha: true },
       rules: {},
     },
   ],
