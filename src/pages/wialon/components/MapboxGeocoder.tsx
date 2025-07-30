007100eedfcdc5a04bbf8218a8c0cdb14f1ebcb2
Heinrich Nel <heinrich@matanuska.co.za>
2025-07-30 14:33:55 +0000

    Fix VendorPage formatting

--- src/pages/wialon/components/MapboxGeocoder.tsx
+++ src/pages/wialon/components/MapboxGeocoder.tsx
@@ -1,78 +1,33 @@
-// Import with require to bypass TypeScript module resolution issues
-// @ts-ignore
-const MapboxGeocoder = require("@mapbox/mapbox-gl-geocoder");
-import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
-import mapboxgl from "mapbox-gl";
-import React from "react";
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
-// Define control position type explicitly since there might be type issues
-type ControlPosition = "top-left" | "top-right" | "bottom-left" | "bottom-right";
+const MAPBOX_TOKEN = 'jou-token-hier';
 
-interface MapboxGeocoderProps {
-  map: mapboxgl.Map;
-  options?: {
-    accessToken: string;
-    mapboxgl: typeof mapboxgl;
-    marker?: boolean;
-    placeholder?: string;
-    [key: string]: any;
-  };
-  onResult?: (event: { result: any }) => void;
-  onError?: (event: any) => void;
-  position?: ControlPosition;
-}
+export default function MapboxGeocoderComponent() {
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
 
-/**
- * Add Mapbox Geocoder Component
- *
- * This component wraps the Mapbox Geocoder functionality
- * and provides React integration for the Mapbox Geocoder control.
- */
-class MapboxGeocoderComponent extends React.Component<MapboxGeocoderProps> {
-  private geocoder: any = null;
-  private mapboxGeocoderContainerRef = React.createRef<HTMLDivElement>();
-
-  componentDidMount() {
-    const { map, options, onResult, onError } = this.props;
-
-    this.geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken || "",
-      mapboxgl,
-      ...options,
+  useEffect(() => {
+    mapboxgl.accessToken = MAPBOX_TOKEN;
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current!,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [25, -29],
+      zoom: 5,
     });
 
-    if (onResult && this.geocoder) {
-      this.geocoder.on("result", onResult);
-    }
+    // Die truuk is om *nie* "new MapboxGeocoder" te probeer as jy 'n namespace gebruik nie
+    // Gebruik die .default as jy via import * werk:
+    // @ts-ignore
+    const geocoder = new (MapboxGeocoder as any).default({
+      accessToken: MAPBOX_TOKEN,
+      mapboxgl: mapboxgl,
+    });
 
-    if (onError && this.geocoder) {
-      this.geocoder.on("error", onError);
-    }
+    map.addControl(geocoder);
 
-    // Add the geocoder to the map
-    if (map && this.geocoder) {
-      // Use the position prop if provided, otherwise default to 'top-left'
-      const controlPosition = this.props.position || "top-left";
-      map.addControl(this.geocoder, controlPosition);
-    } else {
-      console.warn("Map is not available for the geocoder");
-    }
-  }
+    return () => map.remove();
+  }, []);
 
-  componentWillUnmount() {
-    const { map, onResult, onError } = this.props;
-
-    // Clean up
-    if (this.geocoder) {
-      if (onResult) this.geocoder.off("result", onResult);
-      if (onError) this.geocoder.off("error", onError);
-      if (map) map.removeControl(this.geocoder);
-    }
-  }
-
-  render() {
-    return <div ref={this.mapboxGeocoderContainerRef} />;
-  }
+  return <div ref={mapContainerRef} style={{ width: '100%', height: '400px' }} />;
 }
-
-export default MapboxGeocoderComponent;
