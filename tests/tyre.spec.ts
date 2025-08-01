524b73277058ce5ab12cb38dba4d7b26f0502083
Heinrich Nel <heinrich@matanuska.co.za>
2025-08-01 18:02:13 +0000

    Save work and cleanup before freeing disk space

--- tests/tyre.spec.ts
+++
@@ -1,9 +0,0 @@
-
-import { test, expect } from "@playwright/test";
-
-test("Delete a tyre from the list", async ({ page }) => {
-  await page.goto("http://localhost:5173/tyres");
-  await page.getByRole("button", { name: /delete/i }).first().click();
-  await page.getByRole("dialog").getByRole("button", { name: /confirm/i }).click();
-  await expect(page.getByText("FM66")).not.toBeVisible();
-});
